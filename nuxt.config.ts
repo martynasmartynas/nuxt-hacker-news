af946f12d94ef5790b23f0d7809398a0925b50eb
Martynas <martynas.brikys@gmail.com>
2026-01-11 23:55:07 +0100

    add scss and prttier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
-  modules: [
-    '@nuxt/a11y',
-    '@nuxt/eslint',
-    '@nuxt/hints',
-    '@nuxt/icon',
-    '@nuxt/test-utils'
-  ]
+  modules: ['@nuxt/a11y', '@nuxt/eslint', '@nuxt/hints', '@nuxt/icon', '@nuxt/test-utils'],
+  css: ['@/assets/styles/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/styles/_variables.scss" as *;'
+        }
+      }
+    }
+  }
 })
